c130bab168f8ea32d7168fba7575cb8594f36243
SHREYA248BHATIA <shreya2408bhatia@gmail.com>
2021-10-27 15:14:15 +0530

    Updates

---
+++ precache-manifest.bd01cdd1766d85746ceb4b3087a1b1fe.js
@@ -0,0 +1,18 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "be1ad1d7b6271536fc7f6f630cce14c5",
+    "url": "/keeper-app/index.html"
+  },
+  {
+    "revision": "d7249aa2e2c6e9d63633",
+    "url": "/keeper-app/static/js/2.cc1989e8.chunk.js"
+  },
+  {
+    "revision": "b42a5ada43195bc6d367",
+    "url": "/keeper-app/static/js/main.b327bfa0.chunk.js"
+  },
+  {
+    "revision": "ed9243ad83c12b8fd98c",
+    "url": "/keeper-app/static/js/runtime-main.604284a8.js"
+  }
+]);
